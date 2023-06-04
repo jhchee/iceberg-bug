341382d71e7ba8478c1dd0393c72784701f7bc32
jhchee <cheehong05090@gmail.com>
2023-06-04 13:07:53 +0800

    Update comment

--- src/main/java/github/jhchee/ScdExample1.java
+++ src/main/java/github/jhchee/ScdExample1.java
@@ -15,16 +15,19 @@ public class ScdExample1 extends IcebergWrapper {
     public static void main(String[] args) {
         SparkSession spark = getSession("ScdExample1");
 
-        spark.sql("CREATE TABLE default.user_scd ( " +
-                "user_id string, " +
-                "first_name string, " +
-                "last_name string, " +
-                "valid_from timestamp, " +
-                "valid_to timestamp, " +
-                "is_current boolean, " +
-                "checksum string " +
-                ") " +
-                "USING iceberg");
+        // create if not exists
+        if (spark.catalog().tableExists("default.user_scd")) {
+            spark.sql("CREATE TABLE default.user_scd ( " +
+                    "user_id string, " +
+                    "first_name string, " +
+                    "last_name string, " +
+                    "valid_from timestamp, " +
+                    "valid_to timestamp, " +
+                    "is_current boolean, " +
+                    "checksum string " +
+                    ") " +
+                    "USING iceberg");
+        }
 
         List<Row> rows = Arrays.asList(
                 RowFactory.create("9612e6e0-2a7f-4198-a617-ad5b5f8e8307", "Mike", "Smith"),
@@ -56,8 +59,16 @@ public class ScdExample1 extends IcebergWrapper {
         Dataset<Row> stagedUpdates = newUserToInsert.selectExpr("NULL as merge_key", "updates.*")
                                                     .union(updates.selectExpr("user_id as merge_key", "*"));
         stagedUpdates.createOrReplaceTempView("staged_updates");
-        stagedUpdates.show();
+        stagedUpdates.show(); // print the followings
+//      +--------------------+--------------------+----------+---------+--------------------+--------+----------+--------------------+
+//      |           merge_key|             user_id|first_name|last_name|          valid_from|valid_to|is_current|            checksum|
+//      +--------------------+--------------------+----------+---------+--------------------+--------+----------+--------------------+
+//      |9612e6e0-2a7f-419...|9612e6e0-2a7f-419...|      Mike|    Smith|2023-06-04 13:06:...|    null|      true|a9b326df4e6da61c5...|
+//      |679226b6-a401-4c9...|679226b6-a401-4c9...|       Bob|    Clark|2023-06-04 13:06:...|    null|      true|75c62a555f786da13...|
+//      |4ede8f3e-f5f9-4a5...|4ede8f3e-f5f9-4a5...|      null|     null|2023-06-04 13:06:...|    null|      true|                null|
+//      +--------------------+--------------------+----------+---------+--------------------+--------+----------+--------------------+
 
+        // ERROR thrown
         spark.sql(
                 "MERGE INTO default.user_scd AS target " +
                 "USING (SELECT * FROM staged_updates) " +
@@ -67,6 +78,5 @@ public class ScdExample1 extends IcebergWrapper {
                 "WHEN NOT MATCHED THEN INSERT * "
         );
 
-        spark.sql("DROP TABLE IF EXISTS default.user_scd");
     }
 }
